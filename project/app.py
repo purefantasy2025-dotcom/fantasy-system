b45a750c8c7f52750314b06362d0d34975738985
purefantasy2025-dotcom <purefantasy2025@gmail.com>
2026-06-05 05:34:19 +0300

    Update app.py

--- project/app.py
+++ project/app.py
@@ -281,5 +281,7 @@ def logout():
     return redirect(url_for('index'))
 
 # RUN APP
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True
